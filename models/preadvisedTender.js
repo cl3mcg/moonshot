6117791bf467a1e22357347dc80af0777ed15947
clemcg <clementcg@outlook.com>
2021-12-20 15:58:58 +0100

    Init

---
+++ models/preadvisedTender.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const preAdvisedTenderSchema = new Schema({
+    companyName: String,
+    sugarID: String,
+    expectedReceiveDate: {
+        type: Date,
+        min: "2021-12-01"
+    },
+    transportMode: Array,
+    airFreightVolume: {
+        type: Number,
+        min: 0
+    },
+    seaFreightFCLVolume: {
+        type: Number,
+        min: 0
+    },
+    seaFreightLCLVolume: {
+        type: Number,
+        min: 0
+    },
+    railFreightVolume: {
+        type: Number,
+        min: 0
+    },
+    keyTradelanes: Array,
+    history: Array,
+    existingCustomerSegment: String,
+    additionalComment: String,
+    countryLocation: String,
+})
+
+module.exports = mongoose.model("PreadvisedTender", preAdvisedTenderSchema)
