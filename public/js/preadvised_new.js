414db501d221961b8be48663f61898f1a6e21848
clemcg <clementcg@outlook.com>
2021-12-28 02:00:21 +0100

    Adding very basic JS

---
+++ public/js/preadvised_new.js
@@ -0,0 +1,16 @@
+// Declaration of fields
+const companyNameFld = document.querySelector("#companyName")
+
+
+// Declaration of elements
+const confirmBtn = document.querySelector("#confirmBtn")
+
+
+
+const launchModal = function () {
+
+}
+
+
+
+confirmBtn.addEventListener("click", function (event) {event.preventDefault(), launchModal()})
