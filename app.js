0287f5d4e54f7242776702694c6755f9a73c25f1
clemcg <clementcg@outlook.com>
2022-02-23 22:19:39 +0100

    Add additional routes & Tender registration routes

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ const methodOverride = require("method-override")
 const mongoose = require("mongoose")
 const nodemailer = require("nodemailer")
 // const Joi = require("joi") // Joi is exported in its own file called joiSchema.js, no need to require it again here.
-const {preadvisedSchema, officeSchema} = require("./Utilities/joiSchemas.js")
+const {preadvisedSchema, officeSchema} = require("./utilities/joiSchemas.js")
+const fileUpload = require("express-fileupload")
 
 // ----- Extended error class
-const ExpressError = require("./Utilities/ExpressError.js")
+const ExpressError = require("./utilities/expressError.js")
 
 // ----- Database models
 const PreadvisedTender = require("./models/preadvisedTender.js")
 const Office = require("./models/office.js")
+const RegisteredTender = require("./models/registeredTender.js")
 
 // ----- Ressources
 const countriesData = require('./public/ressources/countries.json')
@@ -24,6 +26,8 @@ const monthsData = require('./public/ressources/months.json')
 const tradelanes = require('./public/ressources/tradelanes.json')
 const history = require('./public/ressources/history.json')
 const transportModes = require('./public/ressources/transportModes.json')
+const businessVerticals = require('./public/ressources/businessVerticals.json')
+const freightForwarders = require('./public/ressources/freightForwarders.json')
 
 // ----- Middlewares
 app.use(express.static(path.join(__dirname, 'public')))
@@ -33,9 +37,10 @@ app.use(methodOverride("_method"))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.engine("ejs", ejsMate)
+app.use(fileUpload({createParentPath: true}))
 
 // ----- catchAsync middleware used to handle Async functions errors
-const catchAsync = require('./Utilities/catchAsync.js')
+const catchAsync = require('./utilities/catchAsync.js')
 
 // ----- validatePreadvised middleware used with JOI to validate new preavised tenders according to JOI schema
 const validatePreadvised = function (req, res, next) {
@@ -101,23 +106,23 @@ app.get("/moonshot", function (req, res) {
     res.render("indexHome.ejs")
 })
 
-app.get("/moonshot/start", function (req, res){
+app.get("/start", function (req, res){
     res.render("indexStart.ejs")
 })
 
 // ----- Routes MOONSHOT PREADVISED
 
-app.get("/moonshot/preadvised/start", function(req, res){
+app.get("/preadvise/start", function(req, res){
     res.render("preadvised_start.ejs")
 })
 
-app.get("/moonshot/preadvised/new", function (req, res) {
+app.get("/preadvise/new", function (req, res) {
     res.render("preadvised_new.ejs", {countriesData})
 })
 
-app.post("/moonshot/preadvised/new", validatePreadvised, catchAsync(async function(req,res, next){
+app.post("/preadvise/new", validatePreadvised, catchAsync(async function(req,res, next){
         
-    console.log(`A new TENDER PRE-ADVISE submit has been done with the following data:`)
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} A new TENDER PRE-ADVISE submit has been attempted with the following data:`)
     console.log(req.body)
 
     let companyName = req.body.companyName
@@ -160,8 +165,8 @@ app.post("/moonshot/preadvised/new", validatePreadvised, catchAsync(async functi
         countryLocation: countryLocation,
     })
     await newEntry.save()
-    console.log(`A new TENDER PRE-ADVISE has been registered in the database: ${newEntry}`)
-    res.redirect(`/moonshot/preadvised/${newEntry.id}`)
+    console.log(`${colors.black.bgBrightGreen('* OK *')} A new TENDER PRE-ADVISE has been registered in the database: ${companyName}`)
+    res.redirect(`/preadvise/${newEntry.id}`)
 
         // let from = '"Tender registration" <appareil_en_ligne@outlook.com>'
         // let subject = "Your tender has been pre-advised"
@@ -206,7 +211,7 @@ app.post("/moonshot/preadvised/new", validatePreadvised, catchAsync(async functi
 }))
 
 
-app.get("/moonshot/preadvised", catchAsync(async function (req, res) {
+app.get("/preadvise/index", catchAsync(async function (req, res) {
     const d = new Date()
     const today = new Date(d.getFullYear(), d.getMonth(), d.getDate())
     const d30 = new Date (d.setDate(d.getDate() + 30))
@@ -295,7 +300,7 @@ app.get("/moonshot/preadvised", catchAsync(async function (req, res) {
     res.render("preadvised_index.ejs", {countriesData, monthsData, today, next30days, next90days, allPreadvisedTenders, preadvised_past, preadvised_inM, preadvised_inQ, preadvised_inY, preadvised_inAM, preadvised_inAP, preadvised_inEU})
 }))
 
-app.get("/moonshot/preadvised/:id", catchAsync(async function (req, res) {
+app.get("/preadvise/:id", catchAsync(async function (req, res) {
     let matchingId = req.params.id
     let matchingTender = await PreadvisedTender.findById(matchingId)
     // ----- For debugging purposes
@@ -303,24 +308,25 @@ app.get("/moonshot/preadvised/:id", catchAsync(async function (req, res) {
     res.render("preadvised_show.ejs", {countriesData, monthsData, tradelanes, transportModes, history, matchingTender})
 }))
 
-app.delete("/moonshot/preadvised/:id", catchAsync(async function (req, res) {
+app.delete("/preadvise/:id", catchAsync(async function (req, res) {
     let matchingId = req.params.id
     let matchingTender = await PreadvisedTender.findById(matchingId)
-    console.log("A TENDER PRE-ADVISE has been selected for deletion...")
+    let matchingTenderName = matchingTender.companyName
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} A TENDER PRE-ADVISE has been selected for deletion: ${matchingTenderName}`)
     console.log(matchingTender)
     await PreadvisedTender.findByIdAndDelete(matchingId)
-    console.log("... and has been deleted.")
-    res.redirect("/moonshot/preadvised")
+    console.log(`${colors.black.bgBrightGreen('* OK *')} The TENDER PRE-ADVISE related to "${matchingTenderName}" has been deleted`)
+    res.redirect("/preadvise")
 }))
 
-app.get("/moonshot/preadvised/edit/:id", catchAsync(async function (req, res){
+app.get("/preadvise/edit/:id", catchAsync(async function (req, res){
     let matchingId = req.params.id
     let matchingTender = await PreadvisedTender.findById(matchingId)
     res.render("preadvised_edit.ejs", {countriesData, monthsData, matchingTender})
 }))
 
-app.patch("/moonshot/preadvised/edit/:id", validatePreadvised, catchAsync(async function (req, res){
-    console.log(`A new tender as been selected TO BE UPDATED ${req.body}`)
+app.patch("/preadvise/edit/:id", validatePreadvised, catchAsync(async function (req, res){
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} A TENDER PRE-ADVISE has been selected for update: ${req.body.companyName}`)
 
     let matchingId = req.params.id
     let newCompanyName = req.body.companyName
@@ -361,8 +367,181 @@ app.patch("/moonshot/preadvised/edit/:id", validatePreadvised, catchAsync(async 
         additionalComment: newAdditionalComment,
         countryLocation: newCountryLocation
     })
-    console.log(`The tender related to the company ${newCompanyName} has been UPDATED`)
-    res.redirect(`/moonshot/preadvised/${matchingId}`)
+    console.log(`${colors.black.bgBrightGreen('* OK *')} The TENDER PRE-ADVISE related to "${newCompanyName}" has been updated`)
+    res.redirect(`/preadvise/${matchingId}`)
+}))
+
+// ----- Routes MOONSHOT REGISTRATION
+
+app.get("/register/start", function (req, res) {
+    res.render("register_start.ejs")
+})
+
+app.get("/register/new", function (req, res) {
+    res.render("register_new.ejs", {countriesData, businessVerticals})
+})
+
+app.post("/register/new", async function (req, res) {
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} A new TENDER REGISTRATION submit has been attempted`)
+    console.log(req.body)
+    console.log(req.files)
+
+    let isPreadvised = req.body.isPreadvised
+    let preadviseID
+    if (isPreadvised === "yes") {
+        isPreadvised = true
+        preadviseID = req.body.preadviseID
+    } else {
+        isPreadvised = false
+    }
+    let companyName = req.body.companyName
+    let sugarID = req.body.sugarID
+    let businessVertical = req.body.businessVertical
+    let contactName = req.body.contactName
+    let contactJobTitle = req.body.contactJobTitle
+    let contactEmail = req.body.contactEmail
+    let decisionMaker = req.body.decisionMaker
+    let transportMode = req.body.transportMode
+    if (typeof(transportMode) != "object") {transportMode = [transportMode]}
+    let airFreightVol
+    if(!req.body.airFreightVol) {airFreightVol = 0} else {airFreightVol = req.body.airFreightVol}
+    let seaFreightFCLVol
+    if(!req.body.seaFreightFCLVol) {seaFreightFCLVol = 0} else {seaFreightFCLVol = req.body.seaFreightFCLVol}
+    let seaFreightLCLVol
+    if(!req.body.seaFreightLCLVol) {seaFreightLCLVol = 0} else {seaFreightLCLVol = req.body.seaFreightLCLVol}
+    let railFreightVol
+    if(!req.body.railFreightVol) {railFreightVol = 0} else {railFreightVol = req.body.railFreightVol}
+    let keyTradelanes = req.body.keyTradelanes
+    if (typeof(keyTradelanes) != "object") {keyTradelanes = [keyTradelanes]}
+    let commodity = req.body.commodity
+    let specialHandling = req.body.specialHandling
+    if (typeof(specialHandling) != "object") {specialHandling = [specialHandling]}
+    let linkedRFI = req.body.linkedRFI
+    let deadlineRFI
+    if (linkedRFI === "yes") {
+        linkedRFI = true
+        deadlineRFI = req.body.deadlineRFI
+    } else {
+        linkedRFI = false
+    }
+    let receptionDate = req.body.receptionDate
+    let deadlineRFQ = req.body.deadlineRFQ
+    let decisionDate = req.body.decisionDate
+    let startBusinessDate = req.body.startBusinessDate
+    if (typeof(keyTradelanes) != "object") {keyTradelanes = [keyTradelanes]}
+    let lanesAmount = req.body.lanesAmount
+    let transportationScope = req.body.transportationScope
+    if (typeof(transportationScope) != "object") {transportationScope = [transportationScope]}
+    let ratesValidityAir
+    if (airFreightVol === 0) {ratesValidityAir = null} else {ratesValidityAir = req.body.ratesValidityAir}
+    let ratesValidityFCL
+    if (seaFreightFCLVol === 0) {ratesValidityFCL = null} else {ratesValidityFCL = req.body.ratesValidityFCL}
+    let ratesValidityLCL
+    if (seaFreightLCLVol === 0) {ratesValidityLCL = null} else {ratesValidityLCL = req.body.ratesValidityLCL}
+    let ratesValidityRail
+    if (railFreightVol === 0) {ratesValidityRail = null} else {ratesValidityRail = req.body.ratesValidityRail}
+    let contractPeriod = req.body.contractPeriod
+    let paymentTerms = req.body.paymentTerms
+    let bidRestrictions = req.body.bidRestrictions
+    if (typeof(bidRestrictions) != "object") {bidRestrictions = [bidRestrictions]}
+    let bidRequirements = req.body.bidRequirements
+    if (typeof(bidRestrictions) != "object") {bidRestrictions = [bidRestrictions]}
+    let roundsAmount = req.body.roundsAmount
+    let tenderLaunchMethod = req.body.tenderLaunchMethod
+    let history = req.body.history
+    let existingCustomerSegment
+    if(!req.body.existingCustomerSegment) {existingCustomerSegment = null} else {existingCustomerSegment = req.body.existingCustomerSegment}
+    let visitFrequency = req.body.visitFrequency
+    let visitHistory = req.body.visitHistory
+    let currentServiceProvider = req.body.currentServiceProvider
+    let competitorAmount = req.body.competitorAmount
+    let volumeSplit = req.body.volumeSplit
+    let reasonForTender = req.body.reasonForTender
+    let decisionCritera = req.body.decisionCritera
+    let feedbackAvailable = req.body.feedbackAvailable
+    let potential = req.body.potential
+    let additionalComment = req.body.additionalComment
+    let countryLocation = req.body.countryLocation
+    let documentUpload = []
+    if (!req.files) {documentUpload = null} else {
+        filesUploaded = req.files.fileUpload
+        for (let file of filesUploaded) {documentUpload.push(file.name)}
+    }
+
+    let newEntry = new RegisteredTender({
+        recordDate: currentDateAndTime(),
+        lastModifiedDate: null,
+        isPreadvised: isPreadvised,
+        preadviseID : preadviseID,
+        companyName: companyName,
+        sugarID: sugarID,
+        businessVertical: businessVertical,
+        contactName: contactName,
+        contactJobTitle: contactJobTitle,
+        contactEmail: contactEmail,
+        decisionMaker: decisionMaker,
+        transportMode: transportMode,
+        airFreightVolume: airFreightVol,
+        seaFreightFCLVolume: seaFreightFCLVol,
+        seaFreightLCLVolume: seaFreightLCLVol,
+        railFreightVolume: railFreightVol,
+        commodity: commodity,
+        specialHandling : specialHandling,
+        linkedRFI: linkedRFI,
+        deadlineRFI: deadlineRFI,
+        receptionDate: receptionDate,
+        deadlineRFQ: deadlineRFQ,
+        decisionDate: decisionDate,
+        startBusinessDate: startBusinessDate,
+        keyTradelanes: keyTradelanes,
+        lanesAmount: lanesAmount,
+        transportationScope: transportationScope,
+        ratesValidityAir: ratesValidityAir,
+        ratesValidityFCL: ratesValidityFCL,
+        ratesValidityLCL: ratesValidityLCL,
+        ratesValidityRail: ratesValidityRail,
+        contractPeriod: contractPeriod,
+        paymentTerms: paymentTerms,
+        bidRestrictions: bidRestrictions,
+        bidRequirements: bidRequirements,
+        roundsAmount: roundsAmount,
+        tenderLaunchMethod: tenderLaunchMethod,
+        history: history,
+        existingCustomerSegment: existingCustomerSegment,
+        visitFrequency: visitFrequency,
+        visitHistory: visitHistory,
+        currentServiceProvider: currentServiceProvider,
+        competitorAmount: competitorAmount,
+        volumeSplit: volumeSplit,
+        reasonForTender: reasonForTender,
+        decisionCritera: decisionCritera,
+        feedbackAvailable: feedbackAvailable,
+        documentUpload: documentUpload,
+        potential: potential,
+        additionalComment: additionalComment,
+        countryLocation: countryLocation
+    })
+
+    console.log(newEntry)
+    await newEntry.save()
+
+    if (!req.files) {documentUpload = null} else {
+        filesUploaded = req.files.fileUpload
+        for (let file of filesUploaded) {
+        await file.mv(`./uploads/${newEntry._id}/${file.name}`, (err) => {
+            if (err) {return res.status(500).send(err)}
+            })
+        }
+    }
+
+    console.log(`${colors.black.bgBrightGreen('* OK *')} A new TENDER has been registered in the database: ${companyName}`)
+    res.redirect("/register/start")
+})
+
+app.get("/register/edit/:id", catchAsync(async function (req, res){
+    let matchingId = req.params.id
+    let matchingTender = await RegisteredTender.findById(matchingId)
+    res.render("register_edit.ejs", {countriesData, monthsData, businessVerticals, matchingTender})
 }))
 
 // ----- Routes MOONSHOT OFFICES
@@ -381,7 +560,9 @@ res.render("office_new.ejs", {countriesData, monthsData})
 })
 
 app.post("/moonshot/office/new", validateOffice, catchAsync(async function (req, res) {
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} A new OFFICE submit has been attempted with the following data:`)
     console.log(req.body)
+
     let cca2 = req.body.countryLocation
     let officeSetup = req.body.officeSetup
     let companyName = req.body.companyName
@@ -407,26 +588,27 @@ app.post("/moonshot/office/new", validateOffice, catchAsync(async function (req,
         lng: lng
     })
     await newEntry.save()
+    console.log(`${colors.black.bgBrightGreen('* OK *')} A new OFFICE has been registered in the database: ${companyName}`)
     res.redirect(`/moonshot/office/${newEntry._id}`)
 }))
 
 app.get("/moonshot/office/:id", catchAsync(async function (req, res){
     let matchingId = req.params.id
     let matchingOffice = await Office.findById(matchingId)
-    console.log(matchingOffice)
+    // console.log(matchingOffice)
     res.render("office_show.ejs", {countriesData, monthsData, matchingOffice})
 }))
 
 app.get("/moonshot/office/edit/:id", catchAsync(async function (req, res){
     let matchingId = req.params.id
     let matchingOffice = await Office.findById(matchingId)
-    console.log(matchingOffice)
+    // console.log(matchingOffice)
     res.render("office_edit.ejs", {countriesData, monthsData, matchingOffice})
 }))
 
 app.patch("/moonshot/office/edit/:id", validateOffice, catchAsync(async function (req, res){
+    console.log(`${colors.black.bgBrightCyan('* ATTEMPT *')} An OFFICE has been selected for update: ${req.body.companyName}`)
     let matchingId = req.params.id
-
     let newcca2 = req.body.countryLocation
     let newOfficeSetup = req.body.officeSetup
     let newCompanyName = req.body.companyName
@@ -451,7 +633,7 @@ app.patch("/moonshot/office/edit/:id", validateOffice, catchAsync(async function
         lat: newLat,
         lng: newLng
     })
-    console.log(`The office related to the company ${newCompanyName} has been UPDATED with the following data: ${updatedOffice}`)
+    console.log(`${colors.black.bgBrightGreen('* OK *')} The OFFICE data related to ${newCompanyName} has been UPDATED with the following data: ${updatedOffice}`)
     res.redirect(`/moonshot/office/${matchingId}`)
 }))
 
