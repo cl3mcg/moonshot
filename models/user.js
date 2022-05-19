a0f2c2398f25ea2d24e2572cffce19b06704f547
clemcg <clementcg@outlook.com>
2022-05-19 17:07:29 +0200

    Adding the user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = moongose.model('User', UserSchema);
