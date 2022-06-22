83d182586c9283ad120edae52cc350cd81f32bcc
clemcg <clementcg@outlook.com>
2022-06-22 17:29:16 +0200

    Adding the secret backup file

---
+++ secretsBackup.js
@@ -0,0 +1,31 @@
+// THIS FILE IS A BACKUP FILE OF THE env FILE
+PORT=
+NODE_ENV=
+SESSION_SECRET=
+EMAIL_HOST=
+EMAIL_RECEIVER_EMAIL=
+EMAIL_SENDER_NAME="The Moonshot project" <>
+EMAIL_SENDER_EMAIL=
+EMAIL_SENDER_PASSWORD=
+CELLAR_ADDON_HOST=
+CELLAR_ADDON_KEY_ID=
+CELLAR_ADDON_KEY_SECRET=
+CELLAR_ADDON_REGION=
+CELLAR_BUCKET_NAME=
+MONGODB_ADDON_URI=
+SCALEWAY_S3_ACCESS_KEY=
+SCALEWAY_S3_SECRET_KEY=
+SCALEWAY_S3_BUCKET_NAME=
+SCALEWAY_S3_REGION=
+SCALEWAY_S3_ENDPOINT=
+
+// The following is the backup data of the secret.js file used for development environement.
+const secrets = {
+    testHost: "",
+    testReceiverEmail: '',
+    testSenderName: '"The Moonshot project" <>',
+    testSenderEmail: '',
+    testSenderEmailPassword: '',
+}
+
+// module.exports = secrets;
